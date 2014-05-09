f6a61707814be9e04262bbe4d8d2dfc01507e5ff
Jevgeni Koltšin <jevgeni.koltsin@iglu.ee>
2014-05-10 02:04:08 +0300

    cors filter improvements, maven build script update(tomcat7)

--- src/main/java/com/bibsmobile/filter/CORSFilter.java
+++ src/main/java/com/bibsmobile/filter/CORSFilter.java
@@ -1,27 +1,29 @@
 package com.bibsmobile.filter;
 
-import javax.servlet.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
  * Created by Jevgeni on 9.05.2014.
  */
-public class CORSFilter implements Filter {
+@Component
+public class CORSFilter extends OncePerRequestFilter {
 
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-        HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
-        chain.doFilter(req, res);
-    }
-
-    public void init(FilterConfig filterConfig) {
-    }
-
-    public void destroy() {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) {
+            response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS, DELETE");
+            response.addHeader("Access-Control-Allow-Headers", "X-Requested-With,Origin,Content-Type, Accept");
+        }
+        filterChain.doFilter(request, response);
     }
 
 }
