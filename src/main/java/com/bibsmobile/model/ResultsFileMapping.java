43aada435f0c79daa501d4bdf1069fe02d2b3205
Oleksii Repkin <oleksii.repkin@gmail.com>
2014-05-29 03:28:19 +0300

    AP-8 Need to add RESULTS IMPORT to Event View

--- src/main/java/com/bibsmobile/model/ResultsFileMapping.java
+++ src/main/java/com/bibsmobile/model/ResultsFileMapping.java
@@ -13,30 +13,32 @@ import javax.persistence.Transient;
 import org.hibernate.annotations.Cascade;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.json.RooJson;
 
 @RooJavaBean
-@RooJpaActiveRecord(finders = { "findResultsFileMappingsByResultsFile"})
+@RooJpaActiveRecord(finders = { "findResultsFileMappingsByResultsFile" })
+@RooJson
 public class ResultsFileMapping {
 
-	@OneToMany(cascade = CascadeType.ALL, mappedBy = "resultsFileMapping")
-	private Set<ResultsImport> resultsImport;
-	
-	private String name;
-	@ManyToOne 
-	private ResultsFile resultsFile;
-	private boolean skipFirstRow;
-	private String map;
-	
-	@Transient
-	private List<String> row1 = new ArrayList<String>();
-	@Transient
-	private List<String> row2 = new ArrayList<String>();
-	@Transient
-	private TreeMap<String, String> options = new TreeMap<String,String>();
-	
-	@Override
-	public String toString(){
-		return map;
-	}
-	
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "resultsFileMapping")
+    private Set<ResultsImport> resultsImport;
+
+    private String name;
+    @ManyToOne
+    private ResultsFile resultsFile;
+    private boolean skipFirstRow;
+    private String map;
+
+    @Transient
+    private List<String> row1 = new ArrayList<String>();
+    @Transient
+    private List<String> row2 = new ArrayList<String>();
+    @Transient
+    private TreeMap<String, String> options = new TreeMap<String, String>();
+
+    @Override
+    public String toString() {
+        return map;
+    }
+
 }
