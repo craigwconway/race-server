4384a902b0a45681f63aeeb64f1c84b216f47884
Jevgeni Koltšin <jevgeni.koltsin@iglu.ee>
2014-06-08 01:01:25 +0300

    amazon s3 integration

--- src/main/java/com/bibsmobile/controller/EventPhotoController.java
+++ src/main/java/com/bibsmobile/controller/EventPhotoController.java
@@ -1,21 +1,49 @@
 package com.bibsmobile.controller;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
 import com.bibsmobile.model.Event;
 import com.bibsmobile.model.EventPhoto;
+import com.bibsmobile.model.UploadFile;
+import flexjson.JSONSerializer;
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.roo.addon.web.mvc.controller.json.RooWebJson;
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.roo.addon.web.mvc.controller.json.RooWebJson;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RequestMapping("/eventphotos")
 @Controller
 @RooWebScaffold(path = "eventphotos", formBackingObject = EventPhoto.class)
 @RooWebJson(jsonObject = EventPhoto.class)
 public class EventPhotoController {
-    
+
+    private static final AWSCredentials AWS_CREDENTIALS = new BasicAWSCredentials("AKIAJGFZDMCYWJZZAL2Q", "Hg7Xe+CQuCnooQk6suYD4micA9vOWCXVER0JGTI+");
+    private static final String AWS_BUCKET = "bibstest";
+    private static final String AWS_BUCKET_URL_PREFIX = "http://bibstest.s3.amazonaws.com/";
+
     @RequestMapping(params = "form", produces = "text/html")
     public String createForm(@RequestParam(value = "event", required = true) Long eventId, Model uiModel) {
         EventPhoto eventPhoto = new EventPhoto();
@@ -64,5 +92,49 @@ public class EventPhotoController {
     @ResponseBody
     public String findEventsPhotos(@RequestParam(value = "event") Long eventId) {
         return EventPhoto.toJsonArray(EventPhoto.findEventPhotosByEventId(eventId).getResultList());
+    }
+
+    @RequestMapping(value = "/fileupload", method = RequestMethod.GET)
+    public String getUploadForm(Model model) {
+        model.addAttribute("uploadFile", new UploadFile());
+        return "eventphotos/upload";
+    }
+
+    @RequestMapping(value = "/fileupload", method = RequestMethod.POST)
+    @ResponseBody
+    public String fileUpload(@ModelAttribute("uploadFile") UploadFile uploadFile) throws IOException {
+        MultipartFile file = uploadFile.getFile();
+        Map<String, Object> fileInfo = new HashMap<>();
+        StringBuilder fileName = new StringBuilder();
+        fileName.append(((Long) System.currentTimeMillis()).toString()).append(".").append(FilenameUtils.getExtension(file.getOriginalFilename()));
+        String fileNameStr = fileName.toString();
+        if (file != null && file.getSize() > 0) {
+            uploadFileToS3(file, fileNameStr);
+            fileInfo.put("fileOriginalName", file.getOriginalFilename());
+            fileInfo.put("fileType", file.getContentType());
+            fileInfo.put("fileSize", file.getSize());
+            fileInfo.put("s3FileName", fileNameStr);
+            fileInfo.put("s3Url", AWS_BUCKET_URL_PREFIX.concat(fileNameStr));
+        }
+        return new JSONSerializer().serialize(fileInfo);
+    }
+
+    @RequestMapping(value = "/filedelete", method = RequestMethod.DELETE)
+    public ResponseEntity<String> fileDelete(@RequestParam(value = "key", required = true) String fileName) {
+        deleteFileFromS3(fileName);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    private PutObjectResult uploadFileToS3(MultipartFile file, String fileName) throws IOException {
+        AmazonS3 s3Client = new AmazonS3Client(AWS_CREDENTIALS);
+        ByteArrayInputStream stream = new ByteArrayInputStream(file.getBytes());
+        PutObjectRequest putObjectRequest = new PutObjectRequest(AWS_BUCKET, fileName, stream, new ObjectMetadata());
+        return s3Client.putObject(putObjectRequest);
+    }
+
+    private void deleteFileFromS3(String fileName) {
+        AmazonS3 s3Client = new AmazonS3Client(AWS_CREDENTIALS);
+        DeleteObjectRequest deleteObjectRequest = new DeleteObjectRequest(AWS_BUCKET, fileName);
+        s3Client.deleteObject(deleteObjectRequest);
     }
 }
