b0d34e8184c851980fa9daa8e1995438daf1757c
Jevgeni Koltšin <jevgeni.koltsin@iglu.ee>
2014-06-09 00:40:43 +0300

    added missed file

---
+++ src/main/java/com/bibsmobile/model/UploadFile.java
@@ -0,0 +1,17 @@
+package com.bibsmobile.model;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class UploadFile {
+
+    private MultipartFile file;
+
+    public MultipartFile getFile() {
+        return file;
+    }
+
+    public void setFile(MultipartFile file) {
+        this.file = file;
+    }
+}
+
