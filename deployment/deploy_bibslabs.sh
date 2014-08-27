9a817c6ac91d28f6ccbf816086fa4ec4c58d69e5
Tobias Thiel <tobias011@gmail.com>
2014-08-27 01:37:39 -0700

    bug fix for deployment script after testing

--- deployment/deploy_bibslabs.sh
+++ deployment/deploy_bibslabs.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-scp ../target/bibs-server-0.1.0-SNAPSHOT.war http://bibslabs.co:/var/lib/tomcat6/webapps/bibs-server.war
-ssh bibslabs.co -C "sudo rm -rf /var/lib/tomcat6/webapps/bibs-server; sudo service tomcat6 restart"
+scp ../target/bibs-server-0.1.0.BUILD-SNAPSHOT.war circleci@bibslabs.co:/var/lib/tomcat6/webapps/bibs-server.war
