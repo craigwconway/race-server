9381e9a281ca0fba2053d8195a58e48902c2d8b1
galen <gedanziger@gmail.com>
2014-08-30 00:48:09 +0000

    the first time in history a build script has worked on the second try

--- deployment/deploy_bibslabs.sh
+++ deployment/deploy_bibslabs.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-scp target/bibs-server-0.1.0.BUILD-SNAPSHOT.war circleci@bibslabs.co:/var/lib/tomcat6/webapps/bibs-server.war
+scp target/bibs-server-0.1.0.BUILD-SNAPSHOT.war circleci@bibslabs.co:~/bibs-server.war
+ssh circleci@bibslabs.co -C sudo cp bibs-server.war /var/lib/tomcat6/webapps/bibs-server.war
