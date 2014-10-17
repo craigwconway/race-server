6c40795c0ef0222bcb7b85367676a3041672219f
Tobias Thiel <tobias011@gmail.com>
2014-10-17 18:18:56 +0200

    rest registrations search

---
+++ src/main/java/com/bibsmobile/controller/RegistrationsRestController.java
@@ -0,0 +1,183 @@
+package com.bibsmobile.controller;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.bibsmobile.model.Cart;
+import com.bibsmobile.model.CartItem;
+import com.bibsmobile.model.Event;
+import com.bibsmobile.model.EventCartItem;
+import com.bibsmobile.model.UserProfile;
+import com.bibsmobile.util.JSONUtil;
+
+@RequestMapping("/rest/registrations")
+@Controller
+public class RegistrationsRestController {
+
+	@RequestMapping(value = "/search", method = RequestMethod.GET, headers = "Accept=application/json", produces="application/json")
+	public ResponseEntity<String> search(@RequestParam("event") Long eventId, @RequestParam(value="firstName", required=false) String firstName, @RequestParam(value="lastName", required=false) String lastName, @RequestParam(value="start", required=false) Integer start, @RequestParam(value="count", required=false) Integer count, HttpServletRequest request) {
+		try{
+
+			Event event = Event.findEvent(eventId);
+			if (event == null)
+				return new ResponseEntity<String>(JSONUtil.convertErrorMessage("event not found"), HttpStatus.NOT_FOUND);
+			boolean firstNameGiven = (firstName != null && !firstName.isEmpty());
+			boolean lastNameGiven  = (lastName  != null && !lastName.isEmpty());
+			if (!(firstNameGiven || lastNameGiven))
+				return new ResponseEntity<String>(JSONUtil.convertErrorMessage("firstName or lastName has to be included"), HttpStatus.BAD_REQUEST);
+
+			Set<Cart> carts = new HashSet<Cart>();
+			List<EventCartItem> eventCartItems = EventCartItem.findEventCartItemsByEvent(event).getResultList();
+			if (!eventCartItems.isEmpty()) {
+				List<CartItem> cartItems = CartItem.findCompletedCartItemsByEventCartItems(eventCartItems, true).getResultList();
+				for (CartItem cartItem : cartItems) {
+					carts.add(cartItem.getCart());
+				}
+			}
+
+			List<ShortCart> registrations = new LinkedList<ShortCart>();
+			for (Cart c : carts) {
+				ShortCart r = new ShortCart(c);
+				boolean foundUser = false;
+				for (ShortCart.ShortCartItem ci : r.getCartItems()) {
+					if (firstNameGiven && lastNameGiven)
+						foundUser = ci.getUser().getFirstName() != null && ci.getUser().getFirstName().equalsIgnoreCase(firstName) &&
+								ci.getUser().getLastName() != null && ci.getUser().getLastName().equalsIgnoreCase(lastName);
+					else if (firstNameGiven)
+						foundUser = ci.getUser().getFirstName() != null && ci.getUser().getFirstName().equalsIgnoreCase(firstName);
+					else if (lastNameGiven)
+						foundUser = ci.getUser().getLastName() != null && ci.getUser().getLastName().equalsIgnoreCase(lastName);
+				}
+				if (foundUser) registrations.add(r);
+			}
+
+			return new ResponseEntity<String>(JSONUtil.convertPaginated(start, count, registrations), HttpStatus.OK);
+
+		} catch(Exception e) {
+			return new ResponseEntity<String>(JSONUtil.convertException(e), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	/**
+	 * JSON output objects
+	 * all the getters are needed by the Jackson json serializer, but the compiler doesn't know that
+	 *
+	 */
+	private static class ShortCart {
+		private final Long id;
+		private final Date created;
+		private final double total;
+		private final int status;
+		private final List<ShortCartItem> cartItems;
+
+		public ShortCart(Cart c) {
+			this.id = c.getId();
+			this.created = c.getCreated();
+			this.total = c.getTotal();
+			this.status = c.getStatus();
+			this.cartItems = new LinkedList<ShortCartItem>();
+			for (CartItem ci : c.getCartItems()) {
+				this.cartItems.add(new ShortCartItem(ci));
+			}
+		}
+		@SuppressWarnings("unused")
+		public Long getId() {
+			return this.id;
+		}
+		@SuppressWarnings("unused")
+		public Date getCreated() {
+			return this.created;
+		}
+		@SuppressWarnings("unused")
+		public double getTotal() {
+			return this.total;
+		}
+		@SuppressWarnings("unused")
+		public int getStatus() {
+			return this.status;
+		}
+		public List<ShortCartItem> getCartItems() {
+			return this.cartItems;
+		}
+
+		private static class ShortUser {
+			private final Long id;
+			private final String firstName;
+			private final String lastName;
+			private final Date birthDate;
+
+			public ShortUser(UserProfile u) {
+				this.id = u.getId();
+				this.firstName = u.getFirstname();
+				this.lastName = u.getLastname();
+				this.birthDate = u.getBirthdate();
+			}
+			@SuppressWarnings("unused")
+			public Long getId() {
+				return this.id;
+			}
+			public String getFirstName() {
+				return this.firstName;
+			}
+			public String getLastName() {
+				return this.lastName;
+			}
+			@SuppressWarnings("unused")
+			public Date getBirthdate() {
+				return this.birthDate;
+			}
+		}
+		private static class ShortCartItem {
+			private final Long id;
+			private final ShortUser user;
+			private final int quantity;
+			private final String color;
+			private final String size;
+			private final double price;
+
+			public ShortCartItem(CartItem c) {
+				this.id = c.getId();
+				this.user = new ShortUser(c.getUserProfile());
+				this.quantity = c.getQuantity();
+				this.color = c.getColor();
+				this.size = c.getSize();
+				this.price = c.getPrice();
+			}
+			@SuppressWarnings("unused")
+			public Long getId() {
+				return this.id;
+			}
+			public ShortUser getUser() {
+				return this.user;
+			}
+			@SuppressWarnings("unused")
+			public int getQuantity() {
+				return this.quantity;
+			}
+			@SuppressWarnings("unused")
+			public String getColor() {
+				return this.color;
+			}
+			@SuppressWarnings("unused")
+			public String getSize() {
+				return this.size;
+			}
+			@SuppressWarnings("unused")
+			public double getPrice() {
+				return this.price;
+			}
+		}
+	}
+}
--- src/main/java/com/bibsmobile/util/JSONUtil.java
+++ src/main/java/com/bibsmobile/util/JSONUtil.java
@@ -1,5 +1,7 @@
 package com.bibsmobile.util;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,11 +10,24 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JSONUtil {
 	private static final Logger log = LoggerFactory.getLogger(JSONUtil.class);
+	private static final ObjectMapper objMapper = new ObjectMapper();
 
-	public static String convertException(Exception e) {
-		log.error("json error message", e);
-		ErrorMessage m = new ErrorMessage(e.getMessage());
-		ObjectMapper objMapper = new ObjectMapper();
+	/**
+	 * converts an object to a json object
+	 */
+	public static String convertObject(Object o) {
+		try {
+			return objMapper.writeValueAsString(o);
+		} catch (JsonProcessingException e) {
+			return JSONUtil.convertException(e);
+		}
+	}
+
+	/**
+	 * converts a string to a json error message
+	 */
+	public static String convertErrorMessage(String e) {
+		ErrorMessage m = new ErrorMessage(e);
 		try {
 			return objMapper.writeValueAsString(m);
 		} catch (JsonProcessingException e1) {
@@ -21,8 +36,30 @@ public class JSONUtil {
 		}
 	}
 
+	/**
+	 * converts an exception to a json error message
+	 */
+	public static String convertException(Exception e) {
+		log.error("json error message", e);
+		return JSONUtil.convertErrorMessage(e.getMessage());
+	}
+
+	/**
+	 * converts a list of results to a paginated json object
+	 */
+	public static <T> String convertPaginated(Integer start, Integer count, List<T> results) {
+		try {
+			Pagination<T> p = new Pagination<T>(start, count, results);
+			return JSONUtil.convertObject(p);
+		} catch(Exception e) {
+			return JSONUtil.convertException(e);
+		}
+	}
+
+
+
 	private static class ErrorMessage {
-		private String error;
+		private final String error;
 
 		public ErrorMessage(String error) {
 			this.error = error;
@@ -31,6 +68,38 @@ public class JSONUtil {
 		@SuppressWarnings("unused")
 		public String getError() {
 			return this.error;
+		}
+	}
+
+	private static class Pagination<T> {
+		private int start; // how many results skipped from start
+		private int count; // how many results to return
+		private final List<T> results; // results
+
+		public Pagination(Integer start, Integer count, List<T> results) {
+			if (start == null) this.start = 0;
+			else this.start = start;
+			if (count == null || count > results.size()) this.count = results.size();
+			else this.count = count;
+			this.results = results;
+		}
+
+		@SuppressWarnings("unused")
+		public int getTotal() {
+			return this.results.size();
+		}
+
+		public int getStart() {
+			return this.start;
+		}
+
+		public int getCount() {
+			return this.count;
+		}
+
+		@SuppressWarnings("unused")
+		public List<T> getResults() {
+			return this.results.subList(this.getStart(), this.getStart() + this.getCount());
 		}
 	}
 }
