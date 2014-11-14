416df81b84194b2d8f841a1b5b5a509f9b6f0ded
galen <gedanziger@gmail.com>
2014-11-14 13:09:09 -0800

    Update timeofficialdisplay from white site, and punch patrick in the face

--- src/main/java/com/bibsmobile/controller/RaceResultController.java
+++ src/main/java/com/bibsmobile/controller/RaceResultController.java
@@ -5,11 +5,17 @@ import com.bibsmobile.model.RaceImage;
 import com.bibsmobile.model.RaceResult;
 import com.bibsmobile.model.UserProfile;
 import com.bibsmobile.service.UserProfileService;
+
 import java.io.UnsupportedEncodingException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -208,8 +214,33 @@ public class RaceResultController {
     		raceResult.setTimestart(raceResult.getEvent().getGunTime().getTime());
     		System.out.println("[RESULTS] Computed gun time: " + raceResult.getTimeofficialdisplay());
     	}
-        raceResult.merge();
-        return "redirect:/raceresults/" + encodeUrlPathSegment(raceResult.getId().toString(), httpServletRequest);
+    	// Add logic to handle timeofficialdisplay updates if we have a nontrivial timeofficialdisplay value:
+    	if("" != raceResult.valueOfTimeofficialdisplay()) {
+    		DateFormat timeparser = new SimpleDateFormat("kk:mm:ss");
+    		// Define new time difference as timeofficialdisplay
+    		long newtimediff;
+			try {
+				
+				Date newdatetimediff = timeparser.parse(raceResult.valueOfTimeofficialdisplay());
+				newtimediff = newdatetimediff.getTime() - (long) (newdatetimediff.getTimezoneOffset()*60000);
+	    		System.out.println("newtimediff: " + newtimediff);
+	    		System.out.println("timezone offset: " + (long) (newdatetimediff.getTimezoneOffset()*60));
+
+	    		// Define an old time difference quantity, compare to timeofficialdisplay
+	    		long oldtimediff = raceResult.getTimeofficial()-raceResult.getTimestart();
+	    		System.out.println("oldtimediff: " + oldtimediff);
+	    		long difference = newtimediff - oldtimediff;
+	    		System.out.println("adding: " + difference);
+	    		// adjust timeofficial by the difference between timeofficialdisplay and timeofficial
+	    		raceResult.setTimeofficial(raceResult.getTimeofficial() + newtimediff - oldtimediff);
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	}
+		raceResult.merge();
+    	return "redirect:/raceresults/" + encodeUrlPathSegment(raceResult.getId().toString(), httpServletRequest);
+
     }
 
 	@RequestMapping(value = "/{id}", params = "form", produces = "text/html")
