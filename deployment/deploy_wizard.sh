5f2e5481148df64ff1c3ece6f284329b9509fb6e
galen <gedanziger@gmail.com>
2015-01-08 22:32:24 -0800

    Last CI commit

--- deployment/deploy_wizard.sh
+++ deployment/deploy_wizard.sh
@@ -2,4 +2,4 @@
 scp target/bibs-server-0.1.0.BUILD-SNAPSHOT.war circleci@107.178.219.146:~/bibs-server.war
 scp deployment/test_deploy_steps.sh circleci@107.178.219.146:~
 ssh circleci@107.178.219.146 -C sudo ./test_deploy_steps.sh
-curl -s --user 'api:key-09244ca92420306315f8bf76f587f7fa'  https://api.mailgun.net/v2/bibsmobile.com/messages -F from='ShrugBot <galen@bibsmobile.com>'  -F to=patrick@bibsmobile.com -F to=galen@bibsmobile.com -F to=brandon@bibsmobile.com -F to=bryan@bibsmobile.com -F subject='Bibslabs Server Deploy Notification' -F text='An RFID build has been deployed to develop test server from CircleCI'
+curl -X POST --data-urlencode 'payload={"channel": "#engineering", "username": "ShrugBot", "text": "A build is being pushed to <http://107.178.219.146:8080/bibs-server/|car-punch>. It will be ready for testing shortly.", "icon_emoji": ":ghost:"}' https://hooks.slack.com/services/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
