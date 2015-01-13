988343680456d655939687f79ae058e975e04218
galen <gedanziger@gmail.com>
2015-01-13 11:06:33 -0800

    Initial Licensing commit

---
+++ src/main/java/com/bibsmobile/model/DeviceInfo.java
@@ -0,0 +1,42 @@
+package com.bibsmobile.model;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+
+import javax.persistence.Entity;
+import javax.persistence.Transient;
+
+import org.springframework.beans.factory.annotation.Configurable;
+
+@Configurable
+@Entity
+public class DeviceInfo {
+	
+	private Long runnersUsed;
+	
+	@Transient
+	private byte[] macAddress;
+
+	public Long getRunnersUsed() {
+		return runnersUsed;
+	}
+
+	public void setRunnersUsed(Long runnersUsed) {
+		this.runnersUsed = runnersUsed;
+	}
+
+	public byte[] getMacAddress() {
+		// Get macaddress from system info:
+		InetAddress ip;
+		try {
+			ip = InetAddress.getLocalHost();
+			NetworkInterface network = NetworkInterface.getByInetAddress(ip);
+			byte[] mac = network.getHardwareAddress();
+			return mac;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+	
+}
