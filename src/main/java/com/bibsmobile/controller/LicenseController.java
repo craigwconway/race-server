999a916c3740d813f7a3d4e64386f354e5eb2f4d
galen <gedanziger@gmail.com>
2015-01-25 22:36:51 -0800

    add upload license page, process file upload

--- src/main/java/com/bibsmobile/controller/LicenseController.java
+++ src/main/java/com/bibsmobile/controller/LicenseController.java
@@ -2,8 +2,11 @@ package com.bibsmobile.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.bibsmobile.model.UserProfile;
 import com.bibsmobile.util.UserProfileUtil;
@@ -22,5 +25,11 @@ public class LicenseController {
     public String list( Model uiModel) {
         UserProfile user = UserProfileUtil.getLoggedInUserProfile();
         return "licensing/upload";
+    }
+    @RequestMapping(method = RequestMethod.POST, produces = "text/html")
+    public String htmlLicenseResponse(@ModelAttribute("license") MultipartFile uploadFile, Model uiModel) {
+    	System.out.println("Recieved post of license");
+    	UserProfile user = UserProfileUtil.getLoggedInUserProfile();
+    	return "licensing/success";
     }
 }
