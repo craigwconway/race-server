3638ce209898375a6974edf848b7942b4acdc8d7
galen <gedanziger@gmail.com>
2015-01-28 16:14:32 -0800

    http://media.giphy.com/media/ZCoer0FMvihWg/giphy.gif

--- deployment/deploy_s0.sh
+++ deployment/deploy_s0.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 scp target/bibs-server-0.1.0.BUILD-SNAPSHOT.war circleci@104.154.32.45:~/bibs-server.war
-scp deployment/test_deploy_steps.sh circleci@107.178.219.146:~
+scp deployment/test_deploy_steps.sh circleci@104.154.32.45:~
 ssh circleci@104.154.32.45 -C sudo ./test_deploy_steps.sh
 curl -X POST --data-urlencode 'payload={"channel": "#engineering", "username": "ShrugBot", "text": "A build is being pushed to <http://104.154.32.45:8080/bibs-server/|sake-bomb>. It will be ready for testing shortly.", "icon_emoji": ":ghost:"}' https://hooks.slack.com/services/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
