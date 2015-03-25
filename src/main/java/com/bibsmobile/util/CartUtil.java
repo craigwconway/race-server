f1c97fa25ebe2a38826f02236e535c7faa1306ad
sos0 <so.c.choi@gmail.com>
2015-03-25 15:18:12 -0700

    merge was broken. adding all required library components

--- src/main/java/com/bibsmobile/util/CartUtil.java
+++ src/main/java/com/bibsmobile/util/CartUtil.java
@@ -4,6 +4,7 @@ import com.bibsmobile.job.BaseJob;
 import com.bibsmobile.job.CartExpiration;
 import com.bibsmobile.model.Cart;
 import com.bibsmobile.model.CartItem;
+import com.bibsmobile.model.Event;
 import com.bibsmobile.model.EventCartItem;
 import com.bibsmobile.model.EventCartItemCoupon;
 import com.bibsmobile.model.EventCartItemPriceChange;
