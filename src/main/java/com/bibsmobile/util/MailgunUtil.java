b9fb3b86a10ed4e46c4a34e2b70e418086ed8428
galen <gedanziger@gmail.com>
2015-06-11 00:03:14 -0700

    Using live mailgun domain

--- src/main/java/com/bibsmobile/util/MailgunUtil.java
+++ src/main/java/com/bibsmobile/util/MailgunUtil.java
@@ -82,9 +82,9 @@ public final class MailgunUtil {
     public static boolean sendHTML(String to, String subject, String message) {
         Client client = Client.create();
         client.addFilter(new HTTPBasicAuthFilter("api", "key-09244ca92420306315f8bf76f587f7fa"));
-        WebResource webResource = client.resource("https://api.mailgun.net/v2/sandboxda951656aa49447398e9f8dd2cc9c8b3.mailgun.org/messages");
+        WebResource webResource = client.resource("https://api.mailgun.net/v3/events.bibs.io/messages");
         MultivaluedMapImpl formData = new MultivaluedMapImpl();
-        formData.add("from", "Mailgun Sandbox <postmaster@sandboxda951656aa49447398e9f8dd2cc9c8b3.mailgun.org>");
+        formData.add("from", "bibs <chef@events.bibs.io>");
         formData.add("to", to);
         formData.add("bcc", CC_EMAIL);
         formData.add("subject", subject);
@@ -104,9 +104,9 @@ public final class MailgunUtil {
     private static boolean sendInternal(List<List<String>> toPartitions, String subject, String message) {
         Client client = Client.create();
         client.addFilter(new HTTPBasicAuthFilter("api", "key-09244ca92420306315f8bf76f587f7fa"));
-        WebResource webResource = client.resource("https://api.mailgun.net/v2/sandboxda951656aa49447398e9f8dd2cc9c8b3.mailgun.org/messages");
+        WebResource webResource = client.resource("https://api.mailgun.net/v3/events.bibs.io/messages");
         MultivaluedMapImpl formData = new MultivaluedMapImpl();
-        formData.add("from", "Mailgun Sandbox <postmaster@sandboxda951656aa49447398e9f8dd2cc9c8b3.mailgun.org>");
+        formData.add("from", "bibs <chef@events.bibs.io>");
 
         boolean success = true;
         for (List<String> tos : toPartitions) {
