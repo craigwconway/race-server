4c559a2f7583fd1d2c6ae98126c4a85a1d8149d7
galen <gedanziger@gmail.com>
2015-06-25 10:53:01 -0700

    Swap in different frontends to build type

--- src/main/java/com/bibsmobile/util/BuildType.java
+++ src/main/java/com/bibsmobile/util/BuildType.java
@@ -2,12 +2,17 @@ package com.bibsmobile.util;
 
 public final class BuildType {
 	private static final BuildTypeName typeName = BuildTypeName.MASTER;
+	private static final String frontend = "https://bibs-frontend.herokuapp.com";
 	private static final boolean licensing = false;
 	private static final boolean registration = true;
 	private static final boolean rfid = false;
 	
 	public BuildTypeName getTypeName() {
 		return typeName;
+	}
+	
+	public String getFrontend() {
+		return frontend;
 	}
 	
 	public boolean isLicensing() {
