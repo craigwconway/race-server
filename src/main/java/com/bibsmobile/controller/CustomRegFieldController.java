a9387c2143a97534c45e63154d0872c80c187849
galen <gedanziger@gmail.com>
2015-07-29 16:41:04 -0700

    Bugfix for CustomRegField edit + docs

--- src/main/java/com/bibsmobile/controller/CustomRegFieldController.java
+++ src/main/java/com/bibsmobile/controller/CustomRegFieldController.java
@@ -111,13 +111,58 @@ public class CustomRegFieldController {
         return new ResponseEntity<>(headers, HttpStatus.CREATED);
     }
 
+	/**
+     * @api {put} /regfields Edit Question
+     * @apiGroup regfields
+	 * @apiName Edit Question
+	 * @apiDescription Custom questions to appear on registration forms. 
+	 * These can be visible on all questions, or restricted to a select few. If optional is set true, the customer
+	 * may choose to skip them at checkout. If hidden, they will not be returned to answer at checkout.
+	 * @apiParam {Object} event Event object this CustomRegField belongs to
+	 * @apiParam {Number} event.id Id of event object this CustomRegField belongs to
+     * @apiParam {String} question Question mapped by this CustomRegField
+     * @apiParam {String} [responseSet] Comma-delimited set of responses to use for dropdowns
+     * @apiParam {Object[]} [eventItems] Array of EventCartItem objects to restrict this question to. If null, it appears on all.
+     * @apiParam {Number} [eventItems.id] ID of mapped EventCartItems
+     * @apiParam {Boolean} [optional=false] Whether or not the question is optional
+     * @apiParam {Boolean} [hidden=false] Whether or not this question is hidden
+     * @apiParamExample {json} Sample Hide
+     * 	{
+     * 		"id": 1,
+     * 		"event":{"id":2},
+     * 		"hidden":true,
+     * 		"question":"What is the only real dev language?",
+     * 		"responseSet":"nodejs,nodejs"
+     * 	}
+     * @apiParamExample {json} Sample Edit
+     * 	{
+     * 		"id": 1,
+     * 		"event":{"id":2},
+     * 		"optional":true,
+     * 		"question":"What is the top search engine?",
+     * 		"responseSet":"AOL,AOL",
+     * 		"eventItems":[{"id":1},{"id":2}]
+     * 	}
+     * @apiSuccess (200) {Object} customRegField created CustomRegField object
+     */
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT, headers = "Accept=application/json")
     public ResponseEntity<String> updateFromJson(@RequestBody String json, @PathVariable("id") Long id) {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "application/json");
-        CustomRegField customRegField = CustomRegField.fromJsonToCustomRegField(json);
+        CustomRegField customRegField = CustomRegField.fromJson(json);
         customRegField.setId(id);
         customRegField.setEvent(Event.findEvent(customRegField.getEvent().getId()));
+        HashSet <EventCartItem> eventItems = new HashSet<EventCartItem>();
+        for(EventCartItem eventItem : customRegField.getEventItems()) {
+        	eventItems.add(EventCartItem.findEventCartItem(eventItem.getId()));
+        }
+        if(eventItems.size() > 0) {
+        	customRegField.setAllItems(false);
+        	customRegField.setEventItems(eventItems);
+        } else {
+        	customRegField.setEventItems(null);
+        	customRegField.setAllItems(true);
+        }
         if (customRegField.merge() == null) {
             return new ResponseEntity<>(headers, HttpStatus.NOT_FOUND);
         }
