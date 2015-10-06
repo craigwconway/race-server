84693e52a1f20786700a00c302648881b32accb6
galen <gedanziger@gmail.com>
2015-10-06 16:45:43 -0700

    Paid Question Options Object

---
+++ src/main/java/com/bibsmobile/model/CustomRegFieldResponseOption.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package com.bibsmobile.model;
+
+import java.util.Collection;
+
+import flexjson.JSONSerializer;
+
+/**
+ * Used to hold options to respond to questions
+ * @author galen
+ *
+ */
+public class CustomRegFieldResponseOption {
+	/**
+	 * String holding response to question
+	 */
+	private String response;
+	/**
+	 * Long containing discount to price
+	 */
+	private Long price;
+	
+	/**
+	 * @return the response
+	 */
+	public String getResponse() {
+		return response;
+	}
+	/**
+	 * @param response the response to set
+	 */
+	public void setResponse(String response) {
+		this.response = response;
+	}
+	/**
+	 * @return the price
+	 */
+	public Long getPrice() {
+		return price;
+	}
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(Long price) {
+		this.price = price;
+	}
+	
+    public static String toJsonArray(Collection<CustomRegFieldResponseOption> collection) {
+        return new JSONSerializer().include("*.children").exclude("*.class").serialize(collection);
+    }
+
+}
