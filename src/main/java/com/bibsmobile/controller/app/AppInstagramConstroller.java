6300d512183d949b69075b68ae369d993c932be0
galen <gedanziger@gmail.com>
2015-10-21 16:54:36 -0700

    Instagram test endpoint and webhook receipt

--- src/main/java/com/bibsmobile/controller/app/AppInstagramConstroller.java
+++ src/main/java/com/bibsmobile/controller/app/AppInstagramConstroller.java
@@ -26,7 +26,10 @@ import javax.servlet.http.HttpServletResponse;
 import org.jinstagram.*;
 import org.jinstagram.realtime.SubscriptionResponseObject;
 import org.jinstagram.realtime.SubscriptionUtil;
-
+import org.jinstagram.realtime.InstagramSubscription;
+import org.jinstagram.realtime.SubscriptionType;
+import org.jinstagram.realtime.SubscriptionResponse;
+import org.jinstagram.exceptions.InstagramException;
 /**
  * Used to manage instagram integrations with the bibs service. A user can authorize
  * their instagram account through this or share in one of their photos.
@@ -39,6 +42,31 @@ public class AppInstagramConstroller {
 	private static final String HUB_MODE = "hub.mode";
     private static final String HUB_CHALLENGE = "hub.challenge";
     private static final String HUB_VERIFY_TOKEN = "hub.verify_token";
+    
+    
+    @RequestMapping(value = "/webhook", method = RequestMethod.POST)
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        handlePostRequest(req, resp);
+    }
+
+    @RequestMapping(value = "/webhook", produces = "text/plain", method = RequestMethod.GET)
+    @ResponseBody
+    protected String doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	System.out.println("[Instagram] Processing webhook");
+    	
+    	String hubChallenge = req.getParameter(HUB_CHALLENGE);
+        String hubVerifyToken = req.getParameter(HUB_VERIFY_TOKEN);
+        String hubMode = req.getParameter(HUB_MODE);
+        return hubChallenge;
+        //resp.getWriter().print(hubChallenge);
+        
+        /*
+        handleCallbackURLVerification(req, resp);
+        System.out.println("Response: " + resp);
+        System.out.println("Response headers:" + resp.getHeaderNames());
+        System.out.println("Response  status: " + resp.getStatus());
+        */
+    }   
     
     /**
      * Internal Function to handle getting callback URL from instagram
@@ -97,5 +125,30 @@ public class AppInstagramConstroller {
         }
         body = stringBuilder.toString();
         return body;
+    }
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String subscribe() {
+    	InstagramSubscription igSub = new InstagramSubscription()
+        .clientId("e187dc60c88c4673a187200ad76b469b")
+        .clientSecret("3f8a4c9912f54129be5aef1140b131c6")
+        .object(SubscriptionType.TAGS)
+        .objectId("london")
+        .aspect("media")
+        .callback("https://overmind.bibs.io/bibs-server/app/instagram/webhook")
+        .verifyToken("londonTagSubscription");
+    	System.out.println(igSub);
+    	try {
+        	SubscriptionResponse subscriptionResponse = igSub.createSubscription();
+        	System.out.println("Subscription Response: " + subscriptionResponse + " Data: " + subscriptionResponse.getData());
+        	String subscriptionId = subscriptionResponse.getData().getId();
+        	System.out.println("Created subscription:  " + subscriptionId);
+        	return "Now tracking type"  + subscriptionResponse.getData().getType() + " ID: " + subscriptionResponse.getData().getObject();
+    	} catch (InstagramException e) {
+    		System.out.println(e);
+    	}
+    	return "fuck you";
+
     }
 }
