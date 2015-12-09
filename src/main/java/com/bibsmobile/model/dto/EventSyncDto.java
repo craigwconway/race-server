549adde0c3c504c9d938b3587d5182ff3614d584
galen <gedanziger@gmail.com>
2015-12-09 10:14:48 -0800

    Event Sync Progress

---
+++ src/main/java/com/bibsmobile/model/dto/EventSyncDto.java
@@ -0,0 +1,150 @@
+package com.bibsmobile.model.dto;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Date;
+
+import com.bibsmobile.model.Event;
+import com.bibsmobile.model.EventType;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * This is a data transfer object used in conjunction with {@link Event} objects. 
+ * This is for use in list views, and has information associated 
+ * with selecting an event to learn more about.
+ * @author galen
+ *
+ */
+public class EventSyncDto {
+	
+	/**
+	 * @apiDefine eventSyncDto
+	 * @apiSuccess (200) {Number} id Id of event
+	 * @apiSuccess (200) {String} name Name of event
+	 * @apiSuccess (200) {String} timeStartLocal String containing local start time as: MM/dd/yyyy hh:mm:ss a
+	 * @apiSuccess (200) {String} timezone String containing the timezoneID of the event
+	 * @apiSuccess (200) {Object[]} eventTypes Set of event types in this event
+	 * @apiSuccess (200) {Number} eventTypes.id of this event type
+	 * @apiSuccess (200) {String} eventTypes.typeName name of this event type
+	 * @apiSuccess (200) {String} eventTypes.distance distance of this event type
+	 * @apiSuccess (200) {String} eventTypes.racetype type of this event type
+	 * @apiSuccessExample Single Result Found
+	 * [
+	 * 	{
+	 * 		"id":1,
+	 * 		"name":"Kings Canyon Critical Mass",
+	 * 		"timeStartLocal":"12/01/2015 12:44:20 AM",
+	 * 		"timezone":"America/Los_Angeles",
+	 * 		"eventTypes":
+	 * 		[
+	 * 			{
+	 * 				"id":1,
+	 * 				"typeName":"Run with the Lizards 5k",
+	 * 				"distance":"5k",
+	 * 				"racetype":"Running",
+	 * 				"timeStartLocal":"12/01/2015 12:44:21 AM"
+	 * 			}
+	 * 		],
+	 * 		"organizer":null
+	 * 	}
+	 * ]
+	 */
+	
+	/**
+	 * Constructs a DTO object from a raw {@link Event} object.
+	 * @param event
+	 */
+	EventSyncDto(Event event) {
+		this.id = event.getId();
+		this.name = event.getName();
+		this.timeStartLocal = event.getTimeStartLocal();
+		this.timeStart = event.getTimeStart();
+		this.timezone  = event.getTimezoneID();
+		this.eventTypes = EventTypeDto.fromEventTypes(event.getEventTypes());
+	}
+	
+	private Long id;
+	
+	private String name;
+	
+	private String timeStartLocal;
+	
+	private Date timeStart;
+	
+	private String timezone;
+	
+	private Set <EventTypeDto> eventTypes;
+	
+	public String toJson() {
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			return mapper.writeValueAsString(this);
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static String fromEventToDto(Event event ) {
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			return mapper.writeValueAsString(new EventSyncDto(event));
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}	
+	
+	public static String fromEventsToDtoArray(Collection<Event> events ) {
+		List <EventSyncDto> dtos = new ArrayList <EventSyncDto>();
+		for(Event event : events) {
+			dtos.add(new EventSyncDto(event));
+		}
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			return mapper.writeValueAsString(dtos);
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+
+	/**
+	 * @return the timeStartLocal
+	 */
+	public String getTimeStartLocal() {
+		return timeStartLocal;
+	}
+
+	/**
+	 * @return the eventTypes
+	 */
+	public Set<EventTypeDto> getEventTypes() {
+		return eventTypes;
+	}
+	
+	
+}
--- src/main/java/com/bibsmobile/model/dto/TimeSyncDto.java
+++ src/main/java/com/bibsmobile/model/dto/TimeSyncDto.java
@@ -12,49 +12,22 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
- * This is a data transfer object is used in detailed RaceResult returns. 
- * This should only be used for single results or small collections of results,
- * as it is computationally very expensive.
+ * This object represents a single time recieved in a time sync packet.
  * @author galen
  *
  */
 public class TimeSyncDto {
 	
 	/**
-	 * @apiDefine raceResultDetailDto
-	 * @apiSuccess (200) {Number} id unique ID of result
+	 * @apiDefine timeSyncDto
 	 * @apiSuccess (200) {Number} bib Bib number of result
-	 * @apiSuccess (200) {String} firstname First name of athlete
-	 * @apiSuccess (200) {String} lastname Last name of athlete
-	 * @apiSuccess (200) {Number} age Age of athlete
-	 * @apiSuccess (200) {String} gender Gender of athlete. Either M or F
-	 * @apiSuccess (200) {String} city City of origin of athlete
-	 * @apiSuccess (200) {String} timeofficialdisplay Official Time of athlete
-	 * @apiSuccess (200) {String} team Name of athlete's team
-	 * @apiSuccess (200) {Object[]} images Objects containing each of the images of the athlete
-	 * @apiSuccess (200) {Number} images.id id of race image
-	 * @apiSuccess (200) {String} images.filePath url of image to display
-	 * @apiSuccessExample Single Result Found
-	 * [
-	 * 	{
-	 * 		"id":1,
-	 * 		"bib": 22,
-	 * 		"firstname":"Galen",
-	 * 		"lastname":"Danziger",
-	 * 		"city":"San Francisco",
-	 * 		"age":24,
-	 * 		"gender":"M",
-	 * 		"team": "Shrugrunners Run Club",
-	 * 		"timeofficialdisplay":"00:23:57",
-	 * 		"images":
-	 * 		[
-	 * 			{
-	 * 				"id":1,
-	 * 				"filePath": "http://www.mtv.com/crop-images/2013/09/12/drake.jpg"
-	 * 			}
-	 * 		]
-	 * 	}
-	 * ]
+	 * @apiSuccess (200) {Number} eventTypeId Id of cloud-based event type to sync into
+	 * @apiSuccess (200) {Number} timestamp ms timestamp of recorded time
+	 * @apiSuccessExample Single Time
+	 * {
+	 * 	"bib":1,
+	 * 	"timestamp":144244242
+	 * }
 	 */
 	
 	
