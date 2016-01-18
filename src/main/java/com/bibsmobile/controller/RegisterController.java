9bcfa3d28400f579ce13405129cfc610af872713
galen <gedanziger@gmail.com>
2016-01-17 23:49:12 -0800

    Wire in org + templates for create user

--- src/main/java/com/bibsmobile/controller/RegisterController.java
+++ src/main/java/com/bibsmobile/controller/RegisterController.java
@@ -3,8 +3,10 @@
  */
 package com.bibsmobile.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -18,8 +20,11 @@ import com.bibsmobile.model.Badge;
 import com.bibsmobile.model.RaceResult;
 import com.bibsmobile.model.Series;
 import com.bibsmobile.model.SeriesRegion;
+import com.bibsmobile.model.UserAuthority;
+import com.bibsmobile.model.UserProfile;
 import com.bibsmobile.model.dto.AccountCreationDto;
 import com.bibsmobile.util.SpringJSONUtil;
+import com.bibsmobile.util.UserProfileUtil;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -34,6 +39,10 @@ import java.util.HashMap;
 @RequestMapping("/register")
 @Controller
 public class RegisterController {
+	
+    @Autowired
+    private StandardPasswordEncoder encoder;
+	
 	@RequestMapping(value = "", produces = "text/html")
 	public String startPage(Model uiModel) {
 		return "register/first";
@@ -64,6 +73,16 @@ public class RegisterController {
 	public String submitFourthPage(Model uiModel, AccountCreationDto account) {
 		System.out.println("Submitted step 4");
 		uiModel.addAttribute("account", account);
+		
+		UserProfile user = new UserProfile();
+		user.setUsername(account.getUsername());
+		user.setPassword(encoder.encode(account.getPassword()));
+		user.setPhone(account.getPhone());
+		user.setEmail(account.getEmail());
+		user.setFirstname(account.getFirstname());
+		user.setLastname(account.getLastname());
+		
+		UserProfileUtil.createUser(user, UserAuthority.EVENT_ADMIN, account.getOrgName(), account.getOrgDescription());
 		return "register/created";
 	}
 	
