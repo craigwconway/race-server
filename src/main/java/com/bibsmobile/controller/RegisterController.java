15b4ed94f29f7788c107ff4b29a279e7d404f3c8
galen <gedanziger@gmail.com>
2016-01-17 22:43:52 -0800

    Views link to eachother, end in confirmation view

--- src/main/java/com/bibsmobile/controller/RegisterController.java
+++ src/main/java/com/bibsmobile/controller/RegisterController.java
@@ -7,8 +7,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.bibsmobile.model.Event;
@@ -16,6 +18,7 @@ import com.bibsmobile.model.Badge;
 import com.bibsmobile.model.RaceResult;
 import com.bibsmobile.model.Series;
 import com.bibsmobile.model.SeriesRegion;
+import com.bibsmobile.model.dto.AccountCreationDto;
 import com.bibsmobile.util.SpringJSONUtil;
 
 import java.util.List;
@@ -35,19 +38,33 @@ public class RegisterController {
 	public String startPage(Model uiModel) {
 		return "register/first";
 	}
-	
-	@RequestMapping(value="/step2", produces = "text/html")
-	public String accountPage(Model uiModel) {
+
+	@RequestMapping(value = "first", method= RequestMethod.POST, produces = "text/html")
+	public String submitStartPage(Model uiModel, AccountCreationDto account) {
+		System.out.println("Submitted step 1");
+		uiModel.addAttribute("account", account);
 		return "register/second";
 	}
 	
-	@RequestMapping(value="/step3", produces = "text/html")
-	public String contactPage(Model uiModel) {
+	@RequestMapping(value = "second", method= RequestMethod.POST, produces = "text/html")
+	public String submitSecondPage(Model uiModel, AccountCreationDto account) {
+		System.out.println("Submitted step 2");
+		uiModel.addAttribute("account", account);
 		return "register/third";
 	}
-	
-	@RequestMapping(value="/step4", produces = "text/html")
-	public String orgPage(Model uiModel) {
+
+	@RequestMapping(value = "third", method= RequestMethod.POST, produces = "text/html")
+	public String submitThirdPage(Model uiModel, AccountCreationDto account) {
+		System.out.println("Submitted step 3");
+		uiModel.addAttribute("account", account);
 		return "register/fourth";
-	}	
+	}
+	
+	@RequestMapping(value = "fourth", method= RequestMethod.POST, produces = "text/html")
+	public String submitFourthPage(Model uiModel, AccountCreationDto account) {
+		System.out.println("Submitted step 4");
+		uiModel.addAttribute("account", account);
+		return "register/created";
+	}
+	
 }
